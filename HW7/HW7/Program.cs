e781540f5135e1f3c121c1f084250b7a6faad7a3
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-09-02 20:17:18 +0300

    Added data for test

--- HW7/HW7/Program.cs
+++ HW7/HW7/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ComplexNumberLibrary;
+using GeometricShapesLibrary;
+
+var temp = new ComplexNumber(3, 4);
+var temp2 = new ComplexNumber(1, 2);
+var temp3 = temp + temp2;
+Console.WriteLine($"{temp3.Real} + {temp3.Imaginary}i");
+temp3 = temp - temp2;
+Console.WriteLine($"{temp3.Real} + {temp3.Imaginary}i");
+temp3 = temp * temp2;
+Console.WriteLine($"{temp3.Real} + {temp3.Imaginary}i");
+temp3 = temp / temp2;
+Console.WriteLine($"{temp3.Real} + {temp3.Imaginary}i");
+
+Console.WriteLine(temp == temp2);
+Console.WriteLine(temp != temp2);
+temp = new ComplexNumber(3, 4);
+temp2 = new ComplexNumber(1, 2);
+Console.WriteLine(temp == temp2);
+Console.WriteLine(temp != temp2);
+temp = new ComplexNumber(3, 4);
+temp2 = new ComplexNumber(3, 2);
+Console.WriteLine(temp == temp2);
+Console.WriteLine(temp != temp2);
+temp = new ComplexNumber(3, 5);
+temp2 = new ComplexNumber(2, 5);
+Console.WriteLine(temp == temp2);
+Console.WriteLine(temp != temp2);
+temp = new ComplexNumber(3, 5);
+temp2 = new ComplexNumber(3, 5);
+Console.WriteLine(temp == temp2);
+Console.WriteLine(temp != temp2);
+
+Console.WriteLine(temp.Equals(temp2));
+Console.WriteLine(temp2.GetHashCode());
+
+var circle = new Circle(10);
+Console.WriteLine(circle.Area());
+Console.WriteLine(circle.Perimeter());
+Console.WriteLine(circle.ToString());
+
+var rectangle = new Rectangle(10, 15);
+Console.WriteLine(rectangle.Area());
+Console.WriteLine(rectangle.Perimeter());
+Console.WriteLine(rectangle.ToString());
+
+var triangle = new Triangle(5, 12, 15);
+Console.WriteLine(triangle.Area());
+Console.WriteLine(triangle.Perimeter());
+Console.WriteLine(triangle.ToString());
